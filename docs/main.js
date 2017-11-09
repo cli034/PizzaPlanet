7318266a0894fde186b1967115a2f3c18fa68427
Nam Nguyen <nnguy072@ucr.edu>
2017-11-09 13:58:11 -0800

    checkout items now have a price + total

--- docs/main.js
+++ docs/main.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
   };
 
   var orderSummary = [];
+  var orderPrices = [];
 
   function calcTotalSum(){
     var total = 0;
@@ -23,26 +24,35 @@ $(document).ready(function() {
   function changeOrderSum(){
     if(!(document.getElementById("supremePizzaCheck").checked)){
       var index = orderSummary.indexOf("Supreme Pizza");
-      if(index > -1)
+      if(index > -1){
         orderSummary.splice(index, 2);
+        orderPrices.splice(index, 2);
+      }
     }
     if(!(document.getElementById("pepperoniPizzaCheck").checked)){
       var index = orderSummary.indexOf("Pepperoni Pizza");
-      if(index > -1)
+      if(index > -1){
         orderSummary.splice(index, 2);
+        orderPrices.splice(index, 2);
+      }
     }
     if(!document.getElementById("buffaloWingCheck").checked){
       var index = orderSummary.indexOf("Buffalo Wings");
-      if(index > -1)
+      if(index > -1){
         orderSummary.splice(index, 2);
+        orderPrices.splice(index, 2);
+      }
     }
     if(!document.getElementById("lemonWingCheck").checked){
       var index = orderSummary.indexOf("Lemon Pepper Wings");
-      if(index > -1)
+      if(index > -1){
         orderSummary.splice(index, 2);
+        orderPrices.splice(index, 2);
+      }
     }
 
-    $("#orderSum").html(orderSummary);
+    $("#orderSumItems").html(orderSummary);
+    $("#orderSumPrice").html(orderPrices);
     calcTotalSum();
   }
 
@@ -53,28 +63,32 @@ $(document).ready(function() {
       if(!orderSummary.includes("Supreme Pizza")){
         orderSummary.push("Supreme Pizza");
         orderSummary.push("<br \>");
-        console.log(orderSummary);
+        orderPrices.push("$" + prices["Supreme Pizza"].toString());
+        orderPrices.push("<br \>");
       }
     }
     if(document.getElementById("pepperoniPizzaCheck").checked){
       if(!orderSummary.includes("Pepperoni Pizza")){
         orderSummary.push("Pepperoni Pizza");
         orderSummary.push("<br \>");
-        console.log(orderSummary);
+        orderPrices.push("$" + prices["Pepperoni Pizza"].toString());
+        orderPrices.push("<br \>");
       }
     }
     if(document.getElementById("buffaloWingCheck").checked){
       if(!orderSummary.includes("Buffalo Wings")){
         orderSummary.push("Buffalo Wings");
         orderSummary.push("<br \>");
-        console.log(orderSummary);
+        orderPrices.push("$" + prices["Buffalo Wings"].toString());
+        orderPrices.push("<br \>");
       }
     }
     if(document.getElementById("lemonWingCheck").checked){
       if(!orderSummary.includes("Lemon Pepper Wings")){
         orderSummary.push("Lemon Pepper Wings");
         orderSummary.push("<br \>");
-        console.log(orderSummary);
+        orderPrices.push("$" + prices["Lemon Pepper Wings"].toString());
+        orderPrices.push("<br \>");
       }
     }
     changeOrderSum();
